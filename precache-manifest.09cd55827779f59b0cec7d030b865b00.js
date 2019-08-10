cf455c0547ff69860f83619667d42d1845d72ea0
wuxinguai <wuxinguai@foxmail.com>
2019-08-10 22:39:17 +0800

    init

---
+++ precache-manifest.09cd55827779f59b0cec7d030b865b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7546bac1501526260af6428b2ac4c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89cd7fe840e54847ffcb",
+    "url": "/static/css/2.c92a53cb.chunk.css"
+  },
+  {
+    "revision": "6ce3e6d834735bd81a99",
+    "url": "/static/css/main.1fe8cd5c.chunk.css"
+  },
+  {
+    "revision": "89cd7fe840e54847ffcb",
+    "url": "/static/js/2.d4cf1507.chunk.js"
+  },
+  {
+    "revision": "6ce3e6d834735bd81a99",
+    "url": "/static/js/main.ca6b27cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
